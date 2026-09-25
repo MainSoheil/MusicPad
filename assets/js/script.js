63854d843283265eae1c103c14de3fe2db7d6291
Rasol Tolohey <rasoltolohey@example.com>
2026-09-25 06:32:41 +0330

    Fix sound paths for GitHub Pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 const pads = document.querySelectorAll(".board button");
+const link_prefix = './assets/sound/';
 const sounds = {
-    kick: new Audio('/assets/sound/kick.mp3'),
-    sub: new Audio('/assets/sound/sub.mp3'),
-    snare: new Audio('/assets/sound/snare.mp3'),
-    'hi-hat': new Audio('/assets/sound/hi-hat.mp3'),
-    clap: new Audio('/assets/sound/clap.mp3'),
-    reese: new Audio('/assets/sound/reese.mp3'),
-    wobble: new Audio('/assets/sound/wobble.mp3'),
-    pluck: new Audio('/assets/sound/pluck.mp3'),
-    solo: new Audio('/assets/sound/solo.mp3'),
-    chord: new Audio('/assets/sound/chord.mp3'),
+    kick: new Audio(link_prefix + 'kick.mp3'),
+    sub: new Audio(link_prefix + 'sub.mp3'),
+    snare: new Audio(link_prefix + 'snare.mp3'),
+    'hi-hat': new Audio(link_prefix + 'hi-hat.mp3'),
+    clap: new Audio(link_prefix + 'clap.mp3'),
+    reese: new Audio(link_prefix + 'reese.mp3'),
+    wobble: new Audio(link_prefix + 'wobble.mp3'),
+    pluck: new Audio(link_prefix + 'pluck.mp3'),
+    solo: new Audio(link_prefix + 'solo.mp3'),
+    chord: new Audio(link_prefix + 'chord.mp3'),
 };
 
 pads.forEach(b=>{
